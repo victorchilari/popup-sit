77efca043f36616244d5daabdb07e01e4fe609f0
victorchilari <victor1chilari1@gmail.com>
2021-04-02 22:18:06 +0300

    Add Ant designe and create tabs teamplate, and try to style first tab "Settings"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import {Tabs, Layout} from 'antd';
+// Styles //
+import 'antd/dist/antd.css';
+import './often.css';
+import './App.css';
+// Pages //
+import Settings from './pages/Settings';
+// Extract //
+const {TabPane} = Tabs;
+const {Content} = Layout;
+
+function callback(key: string) {
+	console.log(key);
+}
 
 function App() {
-	return <div className=''></div>;
+	return (
+		<div className='layoutMain'>
+			<Tabs onChange={callback} type='card' defaultActiveKey='4'>
+				<TabPane tab='Main' key='1'>
+					<Content className='paddingContainerInMenu'>
+						<div>Content of Tab Pane 1</div>
+					</Content>
+				</TabPane>
+				<TabPane tab='Details' key='2'>
+					<Content className='paddingContainerInMenu'>
+						<div>Content of Tab Pane 2</div>
+					</Content>
+				</TabPane>
+				<TabPane tab='State' key='3'>
+					<Content className='paddingContainerInMenu'>
+						<div>Content of Tab Pane 3</div>
+					</Content>
+				</TabPane>
+				<TabPane tab='Settings' key='4'>
+					<Content className='paddingContainerInMenu'>
+						<Settings />
+					</Content>
+				</TabPane>
+			</Tabs>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Settings/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Row, Col} from 'antd';
+
+export default function Settings() {
+	return (
+		<div>
+			Hey!
+			<Row>
+				<Col span={6} style={{backgroundColor: 'orange'}}>
+					2 / 5
+				</Col>
+				<Col span={15} offset={3} style={{backgroundColor: 'pink'}}>
+					3 / 5
+				</Col>
+				{/* <Col flex={2} style={{backgroundColor: 'orange'}}>
+					2 / 5
+				</Col>
+				<Col flex={1}>2 / 5</Col>
+				<Col flex={4} style={{backgroundColor: 'pink'}}>
+					3 / 5
+				</Col> */}
+			</Row>
+		</div>
+	);
+}
